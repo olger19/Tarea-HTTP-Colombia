d70cf9218dc4123b683b7ce4b87602ae702e8d0e
OlgerQuispe <goldsag43@gmail.com>
2024-11-30 16:53:09 -0500

    modularizing files

--- lib/paquetes.dart
+++ lib/paquetes.dart
@@ -1,39 +1,54 @@
+import 'package:dart_application_1/classes/pais.dart';
+import 'package:dart_application_1/classes/reqres_respuesta.dart';
 import 'package:http/http.dart' as http;
 
-import 'package:dart_application_1/classes/request_respuesta.dart';
+// import 'classes/reqres_respuesta.dart';
 
-void getRe (){
-  
+void getReqRespService() {
+
+  // final url = 'https://reqres.in/api/users?page=2';
   final url = Uri.parse('https://reqres.in/api/users?page=2');
-  // ya realizamos la peticion y retorna un future 
   http.get(url).then( (res) {
-    //print(res);
-    //final body = jsonDecode(res.body);
+    
+    final resReqRes = ReqResRespuesta.fromJson( res.body );
+    // print(res);
+    // final body = jsonDecode(res.body);
+    // print(body);
+    // print('page: ${ body['page'] }');
+    // print('per_page: ${ body['per_page'] }');
+    // print('id del tercer elemento: ${ body['data'][2]['id'] }');
 
-    //print(body);
-
-    //print('page: ${body['page'] }');
-    //print('per_page: ${body['per_page']}');
-    //print('id dele elemento: ${body['data'][2]['id']}');
-
-    final resReqRes = reqRespuestaFromJson(res.body);
-
-    print('page: ${resReqRes.perPage}');
-    print('per_page: ${resReqRes.perPage}');
-    print('id del tercer elemento: ${resReqRes.data[2].id}');
+    print('page: ${ resReqRes.page }');
+    print('per_page: ${ resReqRes.perPage }');
+    print('id del tercer elemento: ${ resReqRes.data[2].id }');
 
   });
+
 }
 
-void getPais (){
-  
-  final url = Uri.parse('https://restcountries.com/v3.1/alpha/col');
-  // ya realizamos la peticion y retorna un future 
+
+// Tarea:
+
+void getPais() {
+
+  final url = Uri.parse('https://restcountries.eu/rest/v2/alpha/col');
   http.get(url).then( (res) {
     
-    //final col = paisFromJson(res.body);
+    final col = paisFromJson( res.body );
 
-    
-    
-  }).catchError((error) => print(error));
+    print('===========================');
+    print('Pais: ${ col.name }');
+    print('Población: ${ col.population }');
+    print('Fronteras:');
+    col.borders.forEach((f) => print('   $f'));
+    print('Idioma: ${ col.languages[0].nativeName }');
+    print('Lat: ${ col.latlng[0] }');
+    print('Lng: ${ col.latlng[1] }');
+    print('Moneda: ${ col.currencies[0].name }');
+    print('Bandera: ${ col.flag }');
+    print('===========================');
+
+
+  }).catchError( (error){ print(error); });
+
 }
